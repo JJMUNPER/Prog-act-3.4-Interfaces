e11235ad5a131a863b8689798213fc3eae7e8077
Muñoz Perez Juan Jesus <95413106+JJMUNPER@users.noreply.github.com>
2023-01-12 12:19:21 +0100

    Subida final con alguna correcion

--- Editable.java
+++ Editable.java
@@ -4,4 +4,10 @@ public interface Editable {
 
     void establecerContenido (String contenido);
 
+    /**
+     * 8. Lo que se esta haciendo en este apartado es darle mas herencia a la interface imprimible
+     * lo cual no cambia el funcionamiento, simplemente se le esta dando otro recorrido a los datos
+     * que reciben los metodos.
+     */
+
 }
